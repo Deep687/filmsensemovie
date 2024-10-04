32ea2d5fe7c08291c1f086b6f05d64a9d0a39f19
Deep Mankar <deepmankar0@gmail.com>
2024-10-04 11:53:06 +0530

    Resolved bugs in validations

--- src/Views/UpdateProfile.vue
+++ src/Views/UpdateProfile.vue
@@ -1,193 +1,191 @@
 <template>
- 
-    <div class="p-4 bg-black text-white">
-        <h1 class=" semibold p-2 m-2 text-xl md:text-2xl">Update your profile</h1>
-      <form @submit.prevent="handleSubmit" class="space-y-6">
-        <div>
-          <label for="age" class="block text-sm font-medium">Age:</label>
-          <input
-            v-model.number="formData.age"
-            type="number"
-            id="age"
-            class="w-3/5 mt-1 block rounded-md border-gray-300 bg-slate-700 shadow-sm focus:border-teal-500 focus:ring-teal-500 
-            sm:text-sm p-2"  
-            min="18" 
-          />
-          <span v-if="errors.age" class="text-red-500">{{ errors.age }}</span>
-        </div>
-        <div>
-          <label for="gender" class="block text-sm font-medium">Gender:</label>
-          <select
-            v-model="formData.gender"
-            id="gender"
-            class="block font-semibold w-3/5 rounded-md border-gray-300 shadow-sm bg-slate-700 p-2 focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
-          >
-            <option value="" disabled>Select your gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-          <span v-if="errors.gender" class="text-red-500">{{ errors.gender }}</span>
-        </div>
-  
-        <div>
-          <label class="block text-sm font-medium">Hobbies:</label>
-          <div class="flex space-x-4 mt-1 ">
-            <div>
-              <input
-                type="checkbox"
-                id="hobby1"
-                value="reading"
-                v-model="formData.hobbies"
-                class="rounded text-teal-400"
-              />
-              <label for="hobby1" class="ml-2 text-sm">Reading</label>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                id="hobby2"
-                value="traveling"
-                v-model="formData.hobbies"
-                class="rounded text-teal-400"
-              />
-              <label for="hobby2" class="ml-2 text-sm">Traveling</label>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                id="hobby3"
-                value="gaming"
-                v-model="formData.hobbies"
-                class="rounded text-teal-400"
-              />
-              <label for="hobby3" class="ml-2 text-sm">Gaming</label>
-            </div>
-          
-          </div>
-        </div>
-    <span v-if="errors.hobbies" class="text-red-500">{{ errors.hobbies }}</span>
-        <div>
-          <label class="block text-sm font-medium">Subscription:</label>
-          <div class="flex space-x-4 mt-1">
-            <div>
-              <input
-                type="radio"
-                id="subscribe"
-                value="yes"
-                v-model="formData.subscription"
-                class="rounded text-teal-400"
-              />
-              <label for="subscribe" class="ml-2 text-sm">Subscribe to newsletter</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="unsubscribe"
-                value="no"
-                v-model="formData.subscription"
-                class="rounded text-teal-400"
-              />
-              <label for="unsubscribe" class="ml-2 text-sm">Do not subscribe</label>
-            </div>
-          </div>
-          <span v-if="errors.subscription" class="text-red-500">{{ errors.subscription }}</span>
-        </div>
-  
-        <button
-          type="submit"
-          class="w-2/5 bg-teal-400 text-white py-2 px-4 rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+  <div class="p-4 bg-black text-white">
+    <h1 class="semibold p-2 m-2 text-xl md:text-2xl">Update your profile</h1>
+    <form @submit.prevent="handleSubmit" class="space-y-6">
+      <div>
+        <label for="age" class="block text-sm font-medium">Age:</label>
+        <input
+          v-model.number="formData.age"
+          type="number"
+          id="age"
+          class="w-3/5 mt-1 block rounded-md border-gray-300 bg-slate-700 shadow-sm focus:border-teal-500 focus:ring-teal-500 
+          sm:text-sm p-2"  
+          min="18" 
+        />
+        <span v-if="errors.age" class="text-red-500">{{ errors.age }}</span>
+      </div>
+      <div>
+        <label for="gender" class="block text-sm font-medium">Gender:</label>
+        <select
+          v-model="formData.gender"
+          id="gender"
+          class="block font-semibold w-3/5 rounded-md border-gray-300 shadow-sm bg-slate-700 p-2 focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
         >
-          Submit
-        </button>
-      </form>
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted, ref } from 'vue';
-  import * as Yup from 'yup';
-  import { db } from '../utils/firebase';
-  import { auth } from '../utils/firebase';
-  import { doc, setDoc } from 'firebase/firestore';
-  import { useUserData } from '../utils/userData';
+          <option value="" disabled>Select your gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+        <span v-if="errors.gender" class="text-red-500">{{ errors.gender }}</span>
+      </div>
 
-  import { onAuthStateChanged } from 'firebase/auth';
+      <div>
+        <label class="block text-sm font-medium">Hobbies:</label>
+        <div class="flex space-x-4 mt-1 ">
+          <div>
+            <input
+              type="checkbox"
+              id="hobby1"
+              value="reading"
+              v-model="formData.hobbies"
+              class="rounded text-teal-400"
+            />
+            <label for="hobby1" class="ml-2 text-sm">Reading</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="hobby2"
+              value="traveling"
+              v-model="formData.hobbies"
+              class="rounded text-teal-400"
+            />
+            <label for="hobby2" class="ml-2 text-sm">Traveling</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="hobby3"
+              value="gaming"
+              v-model="formData.hobbies"
+              class="rounded text-teal-400"
+            />
+            <label for="hobby3" class="ml-2 text-sm">Gaming</label>
+          </div>
+        </div>
+      <span v-if="errors.hobbies" class="text-red-500">{{ errors.hobbies }}</span>
+      </div>
+      
+      <div>
+        <label class="block text-sm font-medium">Subscription:</label>
+        <div class="flex space-x-4 mt-1">
+          <div>
+            <input
+              type="radio"
+              id="subscribe"
+              value="yes"
+              v-model="formData.subscription"
+              class="rounded text-teal-400"
+            />
+            <label for="subscribe" class="ml-2 text-sm">Subscribe to newsletter</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="unsubscribe"
+              value="no"
+              v-model="formData.subscription"
+              class="rounded text-teal-400"
+            />
+            <label for="unsubscribe" class="ml-2 text-sm">Do not subscribe</label>
+          </div>
+        </div>
+        <span v-if="errors.subscription" class="text-red-500">{{ errors.subscription }}</span>
+      </div>
 
-  const fetchData= async(uid) => {
+      <button
+        type="submit"
+        class="w-2/5 bg-teal-400 text-white py-2 px-4 rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+        :disabled="Object.keys(errors).length > 0"  
+      >
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
 
-if (uid) {
-  await userDataStore.fetchUserData(uid);
+<script setup>
+import { onMounted, ref, watch } from 'vue';
+import * as Yup from 'yup';
+import { db } from '../utils/firebase';
+import { auth } from '../utils/firebase';
+import { doc, setDoc } from 'firebase/firestore';
+import { useUserData } from '../utils/userData';
+import { onAuthStateChanged } from 'firebase/auth';
 
-} else {
-  errorMessage.value = "User not authenticated";
+const fetchData = async (uid) => {
+  if (uid) {
+    await userDataStore.fetchUserData(uid);
+  } else {
+    errorMessage.value = "User not authenticated";
+  }
 }
-}
 
-  const userDataStore = useUserData();
-  const formData = ref({
-    age: null,
-    gender: '',
-    hobbies: [],
-    subscription: '',
-  });
-  const errors = ref({});
-  const currentUser = ref(null);
+const userDataStore = useUserData();
+const formData = ref({
+  age: null,
+  gender: '',
+  hobbies: [],
+  subscription: '',
+});
+const errors = ref({});
+const currentUser = ref(null);
 
-  const schema = Yup.object().shape({
-    age: Yup.number()
-      .required('Age is required')
-      .positive('Age must be a positive number')
-      .min(18, 'You must be at least 18 years old'),
-    gender: Yup.string().required('Gender is required'),
-    hobbies: Yup.array()
-      .min(1, 'At least one hobby is required')
-      .required('Hobbies is required'),
-    subscription: Yup.string().required('Subscription is required'),
-  });
-  
-  
-  onMounted(() => {
-    onAuthStateChanged(auth, async (user) => {
-      currentUser.value = user; 
-  
-      if (user) {
-        await userDataStore.fetchUserData(user.uid);
-        formData.value = { 
-          age: userDataStore.userData.age || null,
-          gender: userDataStore.userData.gender || '',
-          hobbies: userDataStore.userData.hobbies || [],
-          subscription: userDataStore.userData.subscription || '',
-        };
-      }
+const schema = Yup.object().shape({
+  age: Yup.number()
+    .required('Age is required')
+    .positive('Age must be a positive number')
+    .min(18, 'You must be at least 18 years old'),
+  gender: Yup.string().required('Gender is required'),
+  hobbies: Yup.array()
+    .min(1, 'At least one hobby is required')
+    .required('Hobbies is required'),
+  subscription: Yup.string().required('Subscription is required'),
+});
+
+watch(formData, async (newValue) => {
+  errors.value = {}; 
+
+  try {
+    await schema.validate(newValue, { abortEarly: false });
+  } catch (validationErrors) {
+    validationErrors.inner.forEach((error) => {
+      errors.value[error.path] = error.message;
     });
-  });
-  
+  }
+}, { deep: true });
 
-  const handleSubmit = async () => {
-    errors.value = {};
-  
-    try {
-      await schema.validate(formData.value, { abortEarly: false });
-  
-      if (currentUser.value) {
-        const userProfileRef = doc(db, 'users', currentUser.value.uid);
-        await setDoc(userProfileRef, {
-          age: formData.value.age,
-          gender: formData.value.gender,
-          hobbies: formData.value.hobbies,
-          subscription: formData.value.subscription,
-        }, { merge: true });
-        alert('Profile updated successfully!');
-      } else {
-        alert('User not authenticated!');
-      }
+onMounted(() => {
+  onAuthStateChanged(auth, async (user) => {
+    currentUser.value = user;
 
-   fetchData(currentUser.value.uid)} catch (validationErrors) {
-      validationErrors.inner.forEach((error) => {
-        errors.value[error.path] = error.message;
-      });
+    if (user) {
+      await userDataStore.fetchUserData(user.uid);
+      formData.value = {
+        age: userDataStore.userData.age || null,
+        gender: userDataStore.userData.gender || '',
+        hobbies: userDataStore.userData.hobbies || [],
+        subscription: userDataStore.userData.subscription || '',
+      };
     }
-  };
-  </script>
-  
+  });
+});
+
+const handleSubmit = async () => {
+
+  if (Object.keys(errors.value).length === 0 && currentUser.value) {
+    const userProfileRef = doc(db, 'users', currentUser.value.uid);
+    await setDoc(userProfileRef, {
+      age: formData.value.age,
+      gender: formData.value.gender,
+      hobbies: formData.value.hobbies,
+      subscription: formData.value.subscription,
+    }, { merge: true });
+
+    alert('Profile updated successfully!');
+    fetchData(currentUser.value.uid);
+  } else {
+    alert('Please fix the errors before submitting!');
+  }
+};
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,18 @@
 <template>
-  <header class="relative z-10 p-4 flex flex-col md:flex-row justify-between items-center w-full bg-gradient-to-br from-gray-900 to-black bg-opacity-90 shadow-lg">
-    <h1 class="text-4xl font-bold text-transparent bg-clip-text bg-teal-400 mb-4 md:mb-0">
+  <header
+    class="relative z-10 p-4 flex flex-col md:flex-row justify-between items-center w-full bg-gradient-to-br from-gray-900 to-black bg-opacity-90 shadow-lg"
+  >
+    <h1
+      class="text-4xl font-bold text-transparent bg-clip-text bg-teal-400 mb-4 md:mb-0"
+    >
       FilmSense
     </h1>
 
-    <form v-if="currentRoute !== '/'" @submit.prevent="handleSubmit" class="flex w-full max-w-md md:w-1/3 mb-4 relative">
+    <form
+      v-if="currentRoute !== '/'"
+      @submit.prevent="handleSubmit"
+      class="flex w-full max-w-md md:w-1/3 mb-4 relative"
+    >
       <div class="relative w-full">
         <input
           type="text"
@@ -19,25 +27,37 @@
           placeholder="Search movies"
         />
       </div>
-      <ul v-if="searchResults.length && isSuggestionsVisible" class=" absolute left-0 mt-1 w-full bg-gray-800 rounded-lg shadow-lg z-20 max-h-48 overflow-y-auto">
+      <ul
+        v-if="searchResults.length && isSuggestionsVisible"
+        class="absolute left-0 mt-1 w-full bg-gray-800 rounded-lg shadow-lg z-20 max-h-48 overflow-y-auto"
+      >
         <li
           v-for="(result, index) in searchResults"
           :key="index"
-          :class="{'bg-gray-700': index === highlightedIndex}"
+          :class="{ 'bg-gray-700': index === highlightedIndex }"
           @click="selectMovie(result)"
           class="cursor-pointer transition duration-200"
         >
-          <router-link :to="{ name: 'movieDetails', params: { id: result.id } }" class="block px-4 py-2 text-white">
+          <router-link
+            :to="{ name: 'movieDetails', params: { id: result.id } }"
+            class="block px-4 py-2 text-white"
+          >
             {{ result.title }}
           </router-link>
         </li>
       </ul>
     </form>
 
-    <nav v-if="currentRoute !== '/'" class="flex flex-nowrap items-center space-x-4">
+    <nav
+      v-if="currentRoute !== '/'"
+      class="flex flex-nowrap items-center space-x-4"
+    >
       <router-link
         :to="{ path: '/home' }"
-        :class="{ 'text-teal-500': currentRoute === '/home', 'text-white': currentRoute !== '/home' }"
+        :class="{
+          'text-teal-500': currentRoute === '/home',
+          'text-white': currentRoute !== '/home',
+        }"
         class="font-semibold transition duration-300"
       >
         Home
@@ -46,7 +66,10 @@
       <router-link
         v-if="currentRoute !== '/'"
         :to="{ path: '/watchlist' }"
-        :class="{ 'text-teal-500': currentRoute === '/watchlist', 'text-white': currentRoute !== '/watchlist' }"
+        :class="{
+          'text-teal-500': currentRoute === '/watchlist',
+          'text-white': currentRoute !== '/watchlist',
+        }"
         class="flex items-center justify-center p-1 md:p-2 rounded-lg hover:text-teal-500 transition duration-300 ease-in-out"
       >
         <span class="mr-1 md:mr-2">Watchlist</span>
@@ -57,7 +80,7 @@
         </div>
       </router-link>
 
-      <div class="hs-dropdown relative inline-fle ">
+      <div class="hs-dropdown relative inline-fle">
         <button
           @click="toggleDropdown"
           type="button"
@@ -84,92 +107,107 @@
         </button>
 
         <div
-  v-if="isDropdownOpen"
-  class="hs-dropdown-menu transition-opacity duration-300 opacity-100 min-w-36 bg-white shadow-md rounded-lg mt-1 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 z-10 absolute right-0 top-full"
-  role="menu"
-  aria-orientation="vertical"
-  aria-labelledby="hs-dropdown-default"
->
-  <div class="p-1 space-y-0.5 flex flex-col">
-    <span class="text-white text-sm">
-Name : {{ userStore.user.displayName }}
-    </span>
- 
-    <span v-if="userDataStore?.userData?.age" class="text-white text-sm">
-      Age: {{ userDataStore.userData.age }}
-    </span>
+          v-if="isDropdownOpen"
+          class="hs-dropdown-menu transition-opacity duration-300 opacity-100 min-w-36 bg-white shadow-md rounded-lg mt-1 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 z-10 absolute right-0 top-full"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="hs-dropdown-default"
+        >
+          <div ref="target" class="p-1 space-y-0.5 flex flex-col">
+            <span class="text-white text-sm">
+              Name : {{ userStore.user.displayName }}
+            </span>
 
+            <span
+              v-if="userDataStore?.userData?.age"
+              class="text-white text-sm"
+            >
+              Age: {{ userDataStore.userData.age }}
+            </span>
 
-    <span v-if="userDataStore?.userData?.gender" class="text-white text-sm">
-      Gender: {{ userDataStore.userData.gender }}
-    </span>
+            <span
+              v-if="userDataStore?.userData?.gender"
+              class="text-white text-sm"
+            >
+              Gender: {{ userDataStore.userData.gender }}
+            </span>
 
+            <ul
+              v-if="userDataStore?.userData?.hobbies?.length"
+              class="text-white text-sm flex flex-ro flex-wrap"
+            >
+              Hobbies :
+              <li
+                v-for="(hobby, index) in userDataStore.userData.hobbies"
+                :key="index"
+              >
+                {{ hobby }},
+              </li>
+            </ul>
 
-    <ul v-if="userDataStore?.userData?.hobbies?.length" class="text-white text-sm flex flex-ro flex-wrap">
-      Hobbies : 
-      <li v-for="(hobby, index) in userDataStore.userData.hobbies " :key="index" >
-        {{ hobby }},
-      </li>
-    </ul>
+            <router-link :to="{ path: '/updateProfile' }">
+              <button @click="handlebtn">
+                <p
+                  class="bg-teal-500 hover:bg-teal-800 text-white p-2 w-28 rounded-lg text-sm"
+                >
+                  Update profile
+                </p>
+              </button>
+            </router-link>
 
-    <router-link :to="{ path: '/updateProfile' }">
-      <button @click="handlebtn">  <p class="bg-teal-500 hover:bg-teal-800 text-white p-2 w-28 rounded-lg text-sm">
-        Update profile
-      </p></button>
-    
-    </router-link>
-
-    <button
-      @click="handleSignOut"
-      class="bg-red-500 hover:bg-red-800 text-white p-2 w-20 rounded-lg text-sm"
-    >
-      Sign out
-    </button>
-  </div>
-</div>
-
+            <button
+              @click="handleSignOut"
+              class="bg-red-500 hover:bg-red-800 text-white p-2 w-20 rounded-lg text-sm"
+            >
+              Sign out
+            </button>
+          </div>
+        </div>
       </div>
     </nav>
   </header>
 </template>
 
 <script setup>
+import { onClickOutside } from "@vueuse/core";
 import { onAuthStateChanged } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useRouter, useRoute } from 'vue-router';
+import { useRouter, useRoute } from "vue-router";
 import { useUserStore } from "../stores/userStore";
 import { computed, onMounted, ref, watch } from "vue";
 import { fetchSearchMovies } from "../utils/searchMovies";
 import { useWatchlistStore } from "../utils/watchListStore";
 import { useUserData } from "../utils/userData";
-
+const target = ref(null);
 const watchlist = useWatchlistStore();
 const route = useRoute();
 const router = useRouter();
 const userStore = useUserStore();
-const currentUser=ref()
+const currentUser = ref();
 const currentRoute = computed(() => route.path);
 const movieQuery = ref("");
 const searchResults = ref([]);
 const highlightedIndex = ref(-1);
 const isSuggestionsVisible = ref(false);
 const isDropdownOpen = ref(false);
-const errorMessage = ref(''); 
+const errorMessage = ref("");
 
-const handlebtn=()=>{
+const handlebtn = () => {
   isDropdownOpen.value = !isDropdownOpen.value;
-}
+};
+
+onClickOutside(target, handlebtn);
 const fetchSearchData = async (query) => {
   if (!query || query.length < 2) {
     searchResults.value = [];
     isSuggestionsVisible.value = false;
-    return; 
+    return;
   }
   try {
     const searchData = await fetchSearchMovies(query);
     searchResults.value = searchData.results;
-    isSuggestionsVisible.value = true; 
+    isSuggestionsVisible.value = true;
   } catch (err) {
     console.error("Failed to fetch data:", err);
   }
@@ -181,7 +219,7 @@ const toggleDropdown = () => {
 
 const handleFocus = () => {
   if (movieQuery.value.length >= 2) {
-    isSuggestionsVisible.value = true; 
+    isSuggestionsVisible.value = true;
   }
 };
 
@@ -193,14 +231,20 @@ const handleBlur = () => {
 
 const highlightSuggestion = (direction) => {
   if (direction === 1) {
-    highlightedIndex.value = (highlightedIndex.value + 1) % searchResults.value.length; 
+    highlightedIndex.value =
+      (highlightedIndex.value + 1) % searchResults.value.length;
   } else {
-    highlightedIndex.value = (highlightedIndex.value - 1 + searchResults.value.length) % searchResults.value.length; 
+    highlightedIndex.value =
+      (highlightedIndex.value - 1 + searchResults.value.length) %
+      searchResults.value.length;
   }
 };
 
 const selectHighlightedMovie = () => {
-  if (highlightedIndex.value >= 0 && highlightedIndex.value < searchResults.value.length) {
+  if (
+    highlightedIndex.value >= 0 &&
+    highlightedIndex.value < searchResults.value.length
+  ) {
     selectMovie(searchResults.value[highlightedIndex.value]);
   }
 };
@@ -208,63 +252,40 @@ const selectHighlightedMovie = () => {
 const handleSignOut = async () => {
   try {
     await signOut(auth);
-    router.push('/');
+    router.push("/");
   } catch (error) {
-    console.error('Sign-out error: ', error);
+    console.error("Sign-out error: ", error);
   }
 };
 
 const handleSubmit = () => {
-
   if (movieQuery.value) {
     fetchSearchData(movieQuery.value);
-    movieQuery.value = '';
+    movieQuery.value = "";
   }
- 
 };
 
 const selectMovie = (movie) => {
-
   searchResults.value = [];
-  router.push({ name: 'movieDetails', params: { id: movie.id } });
-  movieQuery.value=''
+  router.push({ name: "movieDetails", params: { id: movie.id } });
+  movieQuery.value = "";
 };
 
-const userDataStore = useUserData(); 
+const userDataStore = useUserData();
 
+onMounted(() => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      currentUser.value = user;
 
-
-  
-
-
-  onMounted(() => {
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-          
-            currentUser.value = user;
-
-           userDataStore.fetchUserData(currentUser.value.uid)
-         
-        
-    
-
-        } else {
-      
-            currentUser.value = null;
-        }
-    });
+      userDataStore.fetchUserData(currentUser.value.uid);
+    } else {
+      currentUser.value = null;
+    }
+  });
 });
-
-  
-
-
-
-
 
 watch(movieQuery, (newQuery) => {
   fetchSearchData(newQuery);
 });
-
 </script>
-
-
